189b573436df60c2069a35f5706353b677ad3866
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-04-30 18:03:23 -0400

    chatgpt answer(make 26 divs)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,18 @@
 function App() {
-  return <div>"Hello"</div>
+  return <div className="grid"></div>
+  const AlphabetDivs: React.FC = () => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    return (
+      <div>
+        {alphabet.split('').map((letter, index) => (
+          <div key={index} style={{ width: '50px', height: '50px' }}>
+            {letter}
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App
