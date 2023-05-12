2655d89f3f4cd5d8f806d8410568254a7a134917
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-05-11 20:23:42 -0400

    Add 31 divs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,20 @@
 function App() {
-  return <div className="grid"></div>
-  const AlphabetDivs: React.FC = () => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-    return (
-      <div>
-        {alphabet.split('').map((letter, index) => (
-          <div key={index} style={{ width: '50px', height: '50px' }}>
-            {letter}
-          </div>
-        ))}
-      </div>
-    )
+  function The_Grid() {
+    const Divs = []
+    for (let i = 0; i < 31; i++) {
+      Divs.push(
+        <div key={i} className="w10 h10 bg-blue-500">
+          {i + 1}
+        </div>
+      )
+    }
+    return <div>{Divs}</div>
   }
+  return (
+    <div className="grid">
+      <The_Grid />
+    </div>
+  )
 }
 
 export default App
