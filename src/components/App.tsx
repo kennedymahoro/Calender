fb6fd341fa1887a277964646052542ea0d142a01
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-05-20 21:16:25 -0400

    Calender in a grid pattern.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,15 +3,15 @@ function App() {
     const Divs = []
     for (let i = 0; i < 31; i++) {
       Divs.push(
-        <div key={i} className="w10 h10 bg-blue-500">
+        <div key={i} className="">
           {i + 1}
         </div>
       )
     }
-    return <div>{Divs}</div>
+    return <div className="grid grid-cols-7 gap-y-7">{Divs}</div>
   }
   return (
-    <div className="grid">
+    <div className="w-screen h-screen">
       <The_Grid />
     </div>
   )
