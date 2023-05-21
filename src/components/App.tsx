0a49e74e50b8ca4eeebfa42f21da7260d31d7f2a
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-05-20 22:08:42 -0400

    removed calender component and added CalenderGrid componenet and props.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,8 @@
+import CalenderGrid from './CalenderGrid.tsx'
 function App() {
-  function The_Grid() {
-    const Divs = []
-    for (let i = 0; i < 31; i++) {
-      Divs.push(
-        <div key={i} className="">
-          {i + 1}
-        </div>
-      )
-    }
-    return <div className="grid grid-cols-7 gap-y-7">{Divs}</div>
-  }
   return (
     <div className="w-screen h-screen">
-      <The_Grid />
+      <CalenderGrid Parent="grid grid-cols-7 gap-y-10" Child="" />
     </div>
   )
 }
