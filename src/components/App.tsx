588881588a0bd9bd4ed4e98db72d3b5d5925b8af
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-05-25 12:10:06 -0400

    Added a more pronounce border and removed grid-gap

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,11 @@ import CalenderGrid from './CalenderGrid.tsx'
 function App() {
   return (
     <div className="w-screen h-screen">
-      <CalenderGrid Parent="grid grid-cols-7 gap-y-10" Child="" />
+      <CalenderGrid
+        Parent="grid grid-cols-7"
+        Child="border-2 h-36 hover:bg-sky-200"
+        Days="border-2"
+      />
     </div>
   )
 }
