5d8c8e51364e89cfc45acbaaad0cc13b32e0f440
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-07-07 17:53:34 -0400

    tried to move the number on the top left, but just moved it left.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,6 +6,7 @@ function App() {
         Parent="grid grid-cols-7 w-screen h-screen p-2"
         Child="border-2 h-auto hover:bg-sky-200"
         Days="border-2 h-10"
+        Number="reletive w-fit h-fit insert-x-0 -top-0 bg-black"
       />
     </div>
   )
